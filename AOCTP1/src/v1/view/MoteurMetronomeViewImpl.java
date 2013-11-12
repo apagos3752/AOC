592b9e5e03fb2654f67ccfdcd65749ad09a08d30
Okazari <plouzennec.benjamin@gmail.com>
2013-11-12 09:18:21 -0800

    Avancement de la vue (Semi faite)

--- AOCTP1/src/v1/view/MoteurMetronomeViewImpl.java
+++ AOCTP1/src/v1/view/MoteurMetronomeViewImpl.java
@@ -1,9 +1,21 @@
 package view;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridBagLayout;
+
+import javax.swing.*;
 
 public class MoteurMetronomeViewImpl extends JFrame implements MoteurMetronomeView {
 
+	JButton inc;
+	JButton dec;
+	JButton start;
+	JButton stop;
+	JSlider molette;
+	JPanel tempo;
+	JPanel mesure;
+	
 	public MoteurMetronomeViewImpl(){
 		
 		this.setUp();
@@ -12,9 +24,43 @@ public class MoteurMetronomeViewImpl extends JFrame implements MoteurMetronomeVi
 	
 	public void setUp(){
 		
-		this.setSize(500, 500);
+		this.setSize(400, 300);
 		this.setPreferredSize(getSize());
 		
+		//Initialisation des objets		
+		inc = new JButton("inc");
+		dec = new JButton("dec");
+		start = new JButton("start");
+		stop = new JButton("stop");
+		molette = new JSlider(0, 1, 0);
+		
+		//Mise en place du Layout
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new BorderLayout());
+		//Mise en place du panel principal
+		
+		//Mise en place du Slider
+		molette.setPaintTicks(true);
+		molette.setSize(250, 30);
+		molette.setPreferredSize(molette.getSize());
+		
+		//ajout des composants
+		JPanel southPanel = new JPanel();
+
+		southPanel.setLayout(new BorderLayout());
+		JPanel southPanelBot = new JPanel();
+		JPanel southPanelTop = new JPanel();
+		
+		southPanelBot.add(inc);
+		southPanelBot.add(dec);	
+		southPanelBot.add(start);
+		southPanelBot.add(stop);
+		southPanelTop.add(molette);
+				
+		southPanel.add(southPanelBot, BorderLayout.SOUTH);
+		southPanel.add(southPanelTop, BorderLayout.NORTH);
+		
+		this.add(southPanel,BorderLayout.SOUTH);
 	}
 	
 	@Override
