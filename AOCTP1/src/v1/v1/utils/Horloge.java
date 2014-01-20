7f59b7a2951e245fdfbd3d62a5ea7a726edbfa72
Okazari <plouzennec.benjamin@gmail.com>
2014-01-20 05:39:33 -0800

    Commentaire pour l'horloge

--- AOCTP1/src/v1/v1/utils/Horloge.java
+++ AOCTP1/src/v1/v1/utils/Horloge.java
@@ -2,6 +2,10 @@ package v1.utils;
 
 import v1.controller.Commande;
 
+/**
+ *  Interface à implementer pour construire l'horloge de l'application
+ */
+
 public interface Horloge {
 
 	public void activerPeriodiquement(Commande cmd, double periodeActivation);
--- AOCTP1/src/v1/v1/utils/HorlogeImpl.java
+++ AOCTP1/src/v1/v1/utils/HorlogeImpl.java
@@ -8,14 +8,23 @@ import javax.swing.Timer;
 
 import v1.controller.Commande;
 
+/**
+ *  Horloge de l'application
+ */
 public class HorlogeImpl implements Horloge{
 	
+	//Liste des commandes lancées périodiquement
 	private HashMap<Commande,Timer> hm;
 	
 	public HorlogeImpl() {
 		hm = new HashMap<Commande,Timer>();
 	}
 
+	/**
+	 *  Ajoute une commande à lancer périodiquement
+	 *  @param cmd La commande à lancer périodiquement
+	 *  @param periodeActivation l'écart entre chaque lancement en millisecondes
+	 */
 	@Override
 	public void activerPeriodiquement(final Commande cmd, double periodeActivation) {
 		
@@ -31,6 +40,11 @@ public class HorlogeImpl implements Horloge{
 		
 	}
 
+	/**
+	 *  Lance une commande après un délai
+	 *  @param cmd La commande à lancer 
+	 *  @param delai le délai avant le lancement en millisecondes
+	 */
 	@Override
 	public void activerApresDelai(final Commande cmd, double delai) {
 		
@@ -45,6 +59,10 @@ public class HorlogeImpl implements Horloge{
 		t.start();
 	}
 
+	/**
+	 *  Désactive le lancement périodique d'une commande
+	 *  @param cmd La commande à désactiver
+	 */
 	@Override
 	public void desactiverCommande(Commande cmd) {
 		hm.get(cmd).stop();
