4a04f41442fe09cee5351f3879eeb0058003cd8f
Okazari <plouzennec.benjamin@gmail.com>
2014-01-20 05:17:41 -0800

    Ajout de commentaires sur le launcher

--- AOCTP1/src/v1/v1/launcher/MainTest.java
+++ AOCTP1/src/v1/v1/launcher/MainTest.java
@@ -14,18 +14,22 @@ public class MainTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub		
+		//On crée la vue	
 		MoteurMetronomeViewImpl mmvi = new MoteurMetronomeViewImpl();
+		//On la rend visible
 		mmvi.setVisible(true);
 		
+		//On crée le modèle
 		MoteurMetronomeImpl mmimpl = new MoteurMetronomeImpl();
 		
+		//On crée l'horloge utilisé par l'application
 		Horloge h = new HorlogeImpl();
 		
+		//On met en place l'horloge pour la vue et le modèle
 		mmimpl.setHorloge(h);
 		mmvi.setHorloge(h);
-		//mmimpl.setEtatMarche(true);
-		
+
+		//On crée le controlleur de l'application, qui s'occupera de la lancer.
 		MoteurMetronomeController mcci = new MoteurMetronomeControllerImpl(mmvi,mmimpl);
 	}
 
